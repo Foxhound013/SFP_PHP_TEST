03413f5eb427f7555544ea91699d3d4aeb1d5438
W. Logan Downing <downingstech@gmail.com>
2018-03-01 17:08:38 -0600

    Completed Extended class
    Extended rotate class to the Extend class. Accessed the array and return the last value from Rotate class.

--- src/Rotate.php
+++ src/Rotate.php
@@ -5,21 +5,24 @@
   class Rotate {
 
     var $shiftAmount;
+    var $jsonArray;
+    var $data;
 
     public function __construct($shiftAmount) {
       $this->shiftAmount = $shiftAmount;
+      $this->jsonArray = $this->execute();
     }
 
     public function execute() {
       $filePath = "..\\assets\\rotate.json";
-      return $this->rotateData($filePath);
+      $jsonArray = $this->rotateData($filePath);
+      return $jsonArray;
     }
 
-    function rotateData($filePath) {
+    public function rotateData($filePath) {
       //Load in the json file, and decode into array
       $data = file_get_contents($filePath);
       $data = json_decode($data);
-
       //shift the array to the left
       while ($this->shiftAmount > 0) {
         $tempLast = $data[count($data)-1]; //temporarily hold the last value in the array
@@ -31,11 +34,23 @@
     }
   }
 
+  class Extend extends Rotate {
 
+    //enable the extended class to access the Rotate values
+    public function __construct($shiftAmount) {
+      parent::__construct($shiftAmount);
+      return $this->extend();
+    }
 
+    //Return the last value of the array in Rotate.
+    public function extend() {
+      $data = $this->jsonArray;
+      return $data[count($data)-1];
+    }
+  }
 
-  //$shiftAmount = 1;
+  $shiftAmount = 1;
   $test = new Rotate($shiftAmount);
-  $test->execute();
+  $test2 = new Extend($shiftAmount);
 
  ?>
